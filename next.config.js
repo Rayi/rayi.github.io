4de2ad1daa2a32fe1f869741a318f198fe09e5c6
Rayi <rayisword@gmail.com>
2023-09-06 10:16:21 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    loader: 'custom',
-    loaderFile: './timemit/image/loader.js',
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'timit.quanzi.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  output: 'export'
 }
 
 module.exports = nextConfig
